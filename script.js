fb7cdbca96ebce9275c03a851d60780399126b09
Kuttesch <106610385+Kuttesch@users.noreply.github.com>
2024-06-23 00:27:35 +0200

    fixed popup

--- script.js
+++ script.js
@@ -33,6 +33,21 @@ carouselContent.addEventListener('mouseleave', startAutoScroll);
 startAutoScroll();
 
  */
+// ----- POPUP FUNCTIONALITY -----
+var popup = document.getElementById("popup");
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the popup
+span.onclick = function() {
+    popup.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the popup, close it
+window.onclick = function(event) {
+    if (event.target == popup) {
+        popup.style.display = "none";
+    }
+}
 
 // ----- ON SCROLL FUNCTIONS -----
 window.addEventListener('scroll', function () {
